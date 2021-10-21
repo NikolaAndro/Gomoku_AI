8eecc94ed965413a6bd9311c94fc941ad9385742
NikolaAndro <nikolazeljkoandric@gmail.com>
2021-10-21 14:49:28 -0500

    working on checkBoardStatus

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<array>
 
 using namespace std;
 
@@ -37,6 +39,28 @@ void display(int board[15][15]){
 }
 
 /*
+ * PRECONDITION: Given a 2D array.
+ * POSTCONDITION: Returning if there is a winner in the game. 
+ * DESCRIPTION: Checking columns, diagonals, and rows to see if there is 5 pebbles in the row of the same collor.
+ *              First, finding all positions that contain 1 or 2 on the board. 
+ *              Second, starting from eaach position in the vector, look for 5 consecutive plays by row, column, or any diagonal. 
+ */
+ int checkBoardStatus(int board[15][15]){
+     ///Get all the positions where there is a pebble
+     vector<array<int,2>> current_positions = {};
+     for(int i = 0; i < 15; i++){
+         for(int j=0; j < 15; j++){
+             if(board[i][j] == 1 || board[i][j] == 2 ){
+                 current_positions.push_back({i,j});
+             }
+         }
+     }
+     cout<<"size of the vecotor is "<<current_positions.size()<<endl;
+
+    return 1;
+ }
+
+/*
  * PRECONDITION: Given a 2D array, the color we are playing for, and position we need to change on the board.
  * POSTCONDITION: Changed values on the board
  * DESCRIPTION: Changing teh values on hte board. If it is a black player, it will be represented by 1. 
@@ -46,6 +70,8 @@ void play(int board[15][15], int color, int position[2]){
     //Mark the given position on the board with 1 or 2.
     board[position[0]-1][position[1]-1] = (color == 1) ? 1 : 2;
 }
+
+
 
 int main(){
 
@@ -69,6 +95,7 @@ int main(){
     play(gameBoard, 2, position);
     display(gameBoard);
 
+    int  x = checkBoardStatus(gameBoard);
     
     return 0;
 }
